e331c2ad4f756384f54d7422dbd3128392bd36aa
mnmmelo <mnmmelo@gmail.com>
2015-03-21 02:34:42 +0000

    cachematrix.R code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Utility functions to cache and access a repeatedly used inverse matrix
+## using lexical scoping
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    # Creates list of accessor functions to matrix x and its cached inverse.
+    # 
+    # Args:
+    #    x: a square matrix
+    # Returns:
+    #    List of accessor functions set, get, setinv, getinv
+    
+    # Set the matrix x
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    # Get the matrix x
+    get <- function() x
+    # Set the inverse matrix of x
+    setinv <- function(solve) m <<- solve
+    # Get the inverse matrix of x
+    getinv <- function() m
+    
+    # Return the list of accessor functions
+    list(set = set,
+         get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(accessors, ...) {
+    # Return the inverse matrix
+    # 
+    # Args:
+    #    accessors: a list of accessor functions
+    # Returns:
+    #    The inverse matrix of x
+    
+    # Get the inverse of x
+    invx <- accessors$getinv()
+    if(!is.null(invx)) {
+        message("getting cached data")
+        return(invx)
+    }
+    # Calculate the inverse o x
+    x <- accessors$get()
+    invx <- solve(x, ...)
+    accessors$setinv(invx)
+    
+    invx
 }
+
